d0756459ce0ecadaf313633944135ce218540f79
yamani59 <yamaniyud@gmail.com>
2024-06-10 11:27:27 +0700

    first init

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import { AppProps } from "next/app";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const App = ({ Component, pageProps }: AppProps) => {
+  const [queryClient] = React.useState(() => new QueryClient());
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools />
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </>
+  );
+};
+
+export default App;
