a738a85867755fdf8bcfe47dff6885d2164fdb39
yamani59 <yamaniyud@gmail.com>
2024-06-14 21:27:10 +0700

    feat: setup css mentine

--- components/melecules/profile-dropdown.tsx
+++ components/melecules/profile-dropdown.tsx
@@ -1,7 +1,17 @@
 'use client'
 
-import { forwardRef } from 'react';
-import { Group, Avatar, Text, Menu, UnstyledButton } from '@mantine/core';
+import { FC, forwardRef } from 'react';
+import { IconBaselineDensityMedium } from '@tabler/icons-react';
+import { Group, Avatar, Text, Menu, UnstyledButton, rem } from '@mantine/core';
+
+import {
+  IconSettings,
+  IconSearch,
+  IconPhoto,
+  IconMessageCircle,
+  IconTrash,
+  IconArrowsLeftRight,
+} from '@tabler/icons-react';
 
 interface UserButtonProps extends React.ComponentPropsWithoutRef<'button'> {
   image: string;
@@ -22,25 +32,14 @@ const UserButton = forwardRef<HTMLButtonElement, UserButtonProps>(
       {...others}
     >
       <Group>
+        <IconBaselineDensityMedium  stroke={2} />
         <Avatar src={image} radius="xl" />
-
-        <div style={{ flex: 1 }}>
-          <Text size="sm" fw={500}>
-            {name}
-          </Text>
-
-          <Text c="dimmed" size="xs">
-            {email}
-          </Text>
-        </div>
-
-        {icon}
       </Group>
     </UnstyledButton>
   )
 );
 
-function Demo() {
+const ProfileDropdown : FC = () => {
   return (
     <Menu withArrow>
       <Menu.Target>
@@ -50,9 +49,46 @@ function Demo() {
           email="hspoonlicker@outlook.com"
         />
       </Menu.Target>
-      {/* ... menu items */}
+      
+      <Menu.Dropdown>
+        <Menu.Label>Application</Menu.Label>
+        <Menu.Item leftSection={<IconSettings style={{ width: rem(14), height: rem(14) }} />}>
+          Settings
+        </Menu.Item>
+        <Menu.Item leftSection={<IconMessageCircle style={{ width: rem(14), height: rem(14) }} />}>
+          Messages
+        </Menu.Item>
+        <Menu.Item leftSection={<IconPhoto style={{ width: rem(14), height: rem(14) }} />}>
+          Gallery
+        </Menu.Item>
+        <Menu.Item
+          leftSection={<IconSearch style={{ width: rem(14), height: rem(14) }} />}
+          rightSection={
+            <Text size="xs" c="dimmed">
+              ⌘K
+            </Text>
+          }
+        >
+          Search
+        </Menu.Item>
+
+        <Menu.Divider />
+
+        <Menu.Label>Danger zone</Menu.Label>
+        <Menu.Item
+          leftSection={<IconArrowsLeftRight style={{ width: rem(14), height: rem(14) }} />}
+        >
+          Transfer my data
+        </Menu.Item>
+        <Menu.Item
+          color="red"
+          leftSection={<IconTrash style={{ width: rem(14), height: rem(14) }} />}
+        >
+          Delete my account
+        </Menu.Item>
+      </Menu.Dropdown>
     </Menu>
   );
 }
 
-export default Demo
+export default ProfileDropdown
