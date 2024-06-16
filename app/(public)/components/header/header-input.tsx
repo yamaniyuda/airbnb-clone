cfa06aeb4b1a1dcdafa57d2263bce02ee0f461ce
yamani59 <yamaniyud@gmail.com>
2024-06-16 10:00:33 +0700

    header: add search

--- app/(public)/components/header/header-input.tsx
+++ app/(public)/components/header/header-input.tsx
@@ -1,9 +1,84 @@
+'use client'
+
 import { FC } from "react";
+import React, { useState, useEffect } from 'react';
+import { motion, useAnimation, Variant, Variants } from 'framer-motion';
+import styles from './header-input.module.scss'
+
+
+const headerOptionVariant: Variants = {
+  "close": {
+    opacity: 0,
+    scale: 0.1,
+    transition: {
+      ease: "easeOut",
+      duration: 1
+    }
+  },
+  "open": {
+
+  }
+}
+
+
 
 const HeaderInput: FC = () => {
+  const controls = useAnimation();
+  const [isScrolled, setIsScrolled] = useState(false);
+
+
+  const handleScroll = () => {
+    if (window.scrollY > 50) setIsScrolled(true);
+    else setIsScrolled(false);
+  };
+
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+
+
+  useEffect(() => {
+    if (isScrolled) {
+      controls.start({  transition: { duration: 0.3 } });
+    } else {
+      controls.start({  transition: { duration: 0.3 } });
+    }
+  }, [isScrolled, controls]);
+
+
+
   return (
-    <></>
-  )
+    <div>
+      <motion.div
+        className={styles.search_bar}
+        animate={controls}
+      >
+        <motion.div
+          className={styles.search_bar__option}
+          initial={false}
+          variants={headerOptionVariant}
+          animate={isScrolled ? "close" : "open"}
+        >
+          <span>Stays</span>
+          <span>Experices</span>
+          <span>Online Experices</span>
+        </motion.div>
+        <motion.div>
+          <input type="text" placeholder="Search destinations" />
+          <input type="date" placeholder="Check in" />
+          <input type="date" placeholder="Check out" />
+          <input type="text" placeholder="Add guests" />
+          <button>Search</button>
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+
 }
 
 export default HeaderInput
--- app/(public)/components/header/header.tsx
+++ app/(public)/components/header/header.tsx
@@ -2,15 +2,17 @@ import { FC, Fragment } from "react";
 import styles from './header.module.scss'
 import BrandIcon from "@/components/atoms/brand-icon";
 import ProfileDropdown from "@/components/melecules/profile-dropdown";
+import HeaderInput from "./header-input";
 
 const Header: FC = () => {
   return (
     <Fragment>
       <div className={styles.header}>
         <BrandIcon />
+        <HeaderInput />
         <ProfileDropdown />
       </div>
-      
+      <div style={{ height: '200vh' }} className="bg-gray-400"></div>
     </Fragment>
   );
 };
