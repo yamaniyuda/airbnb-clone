ca91c826efc832e19dc4f0241b69e34058e02cf7
yamani59 <yamaniyud@gmail.com>
2024-06-23 23:53:19 +0700

    fix: error in root request

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -3,6 +3,8 @@ import styles from "./_page.module.scss";
 import Content from "./components/content/content";
 import ProductCard from "@/components/melecules/product-card";
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home({ searchParams }: any) {
   const res = await fetch(process.env.HOSTNAME + "/api/product?")
   const data = await res.json()
