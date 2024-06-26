fce6b48ce029725b2920d5a7f38509ad1a73dd7c
yamani59 <yamaniyud@gmail.com>
2024-06-26 23:26:56 +0700

    fix: error debug

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,18 +1,13 @@
-import { Fragment, Suspense } from "react";
+import { Fragment } from "react";
 import Content from "./components/content/content";
 import Header from "@/components/organisms/header/header";
-import querystring from "querystring";
 
 
 export default async function Home({ searchParams }: any) {
-  const data = await fetch(process.env.HOSTNAME + "/api/product?" + querystring.stringify(searchParams), { cache: 'no-store' })
-                      .then((res) => res?.json())
-                      .catch(_ => { return {} })
-
   return (
     <Fragment>
-      {JSON.stringify(data)}
-      {process.env.HOSTNAME}
+      <Header />
+      <Content searchParams={searchParams} />
     </Fragment>
   );
 }
