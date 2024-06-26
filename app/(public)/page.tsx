e7e9c24a7a377e2f1680af06ce3afe1d6bfa2ff8
yamani59 <yamaniyud@gmail.com>
2024-06-26 23:18:41 +0700

    fix: debug

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,18 +1,18 @@
 import { Fragment, Suspense } from "react";
 import Content from "./components/content/content";
 import Header from "@/components/organisms/header/header";
-import { GetServerSideProps } from "next";
-import { Product } from "../api/product/_mode";
-
-
-
+import querystring from "querystring";
 
 
 export default async function Home({ searchParams }: any) {
+  const data = await fetch(process.env.HOSTNAME + "/api/product?" + querystring.stringify(searchParams), { cache: 'no-store' })
+                      .then((res) => res?.json())
+                      .catch(_ => { return {} })
+
   return (
     <Fragment>
-      <Header />
-      <Content searchParams={searchParams} />
+      {JSON.stringify(data)}
+      {process.env.HOSTNAME}
     </Fragment>
   );
 }
